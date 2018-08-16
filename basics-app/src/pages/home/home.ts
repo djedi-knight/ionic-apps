7766c4b6e06fb29a8a63fb1b3458b54bf6020605
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-08-16 00:36:16 -0400

    Implemented helpful navigation directives

--- basics-app/src/pages/home/home.ts
+++ basics-app/src/pages/home/home.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
 import { UsersPage } from '../users/users';
 
 @Component({
@@ -7,10 +6,5 @@ import { UsersPage } from '../users/users';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) { }
-
-  onGoToUsers() {
-    this.navCtrl.push(UsersPage);
-  }
+  usersPage = UsersPage;
 }

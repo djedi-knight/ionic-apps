f9e82ec3980558f99cb28fe1a66276b808b937a8
Shawn Daichendt <shawn.daichendt@gmail.com>
2018-09-20 13:20:42 -0400

    Updated home component/ template

--- encryption-app/src/pages/home/home.ts
+++ encryption-app/src/pages/home/home.ts
@@ -7,13 +7,31 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  message: string;
 
   constructor(public navCtrl: NavController, private fileEncryption: FileEncryption) {
 
   }
 
   onEncrypt() {
-    this.fileEncryption.encrypt('assets/json/topSecret.json', 'secretKey');
+    this.fileEncryption.encrypt('assets/json/topSecret.json', 'secretKey').then(
+      (value) => {
+        this.message = 'File Encrypted! ' + value;
+      },
+      (error) => {
+        this.message = 'File Encryption Error! ' + error;
+      }
+    );
   }
 
+  onDecrypt() {
+    this.fileEncryption.decrypt('assets/json/topSecret.json', 'secretKey').then(
+      (value) => {
+        this.message = 'File Decrypted! ' + value;
+      },
+      (error) => {
+        this.message = 'File Decryption Error! ' + error;
+      }
+    );
+  }
 }
